7a0fd884074b20cc4dbb7cc8c99e31099ddb54ef
KeystarK550z <mohammadrezaramezanzad@gmail.com>
2019-04-27 14:18:27 +0430

    changing the index.html file to the php version and adding basic wordpress themplate codes

--- index.php
+++ index.php
@@ -4,18 +4,23 @@
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
+    <?php get_language_attributes() ?>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>گروه صنعتی ریحانه | Reyhane food industrials</title>
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css"
-        integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo get_theme_file_uri("./css/styles.css") ?>">
+    <?php
+    wp_head();
+
+
+    wp_styles();
+    ?>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
 
 </head>
 
-<body>
+<body <?php body_class($class) ?>>
 
     <!-- Navigation Menue Section -->
     <nav class="fixed-top row ">
@@ -32,7 +37,7 @@
             </ul>
         </span>
         <!-- Logo -->
-        <span class="col-md-2 col-sm-3 logo"><img src="./sources/logo.png" alt="گروه صنعتی ریحانه "></span>
+        <span class="col-md-2 col-sm-3 logo"><img src=" <?php echo get_theme_file_uri("./sources/logo.png") ?>" alt="گروه صنعتی ریحانه "></span>
         <!-- nav left -->
         <span class="nav-left col-md-5">
             <ul class=" nav flex-row-reverse menu-bg rounded">
@@ -53,21 +58,21 @@
         </ol>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="./sources/mockup.jpg" class="d-block w-100 " alt="...">
+                <img src="<?php echo get_theme_file_uri("./sources/mockup.jpg"); ?>" class="d-block w-100 " alt="...">
                 <div class="carousel-caption d-none d-md-block ">
                     <h5>First slide label</h5>
                     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                 </div>
             </div>
             <div class="carousel-item">
-                <img src="./sources/mockup.jpg" class="d-block w-100" alt="...">
+                <img src="<?php echo get_theme_file_uri("./sources/mockup.jpg"); ?>" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block ">
                     <h5>Second slide label</h5>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </div>
             </div>
             <div class="carousel-item">
-                <img src="./sources/mockup.jpg" class="d-block w-100" alt="...">
+                <img src="<?php echo get_theme_file_uri("./sources/mockup.jpg"); ?>" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block ">
                     <h5>Third slide label</h5>
                     <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
@@ -85,17 +90,14 @@
     </div>
     <!-- Products Section -->
     <div id="products" class="products container ">
-        <h2 id="products-title" class="text-success pt-3 pb-4 "> <i class="fa fa-barcode pr-2"
-                aria-hidden="true"></i>محصولات</h2>
+        <h2 id="products-title" class="text-success pt-3 pb-4 "> <i class="fa fa-barcode pr-2" aria-hidden="true"></i>محصولات</h2>
         <div class="row text-right products">
             <div class="row ">
                 <div class="col-md-4 ">
                     <div class="card mb-4 shadow-sm">
-                        <img src="./sources/tuv-iso-140012004-logo.png" width="100%" height="225"
-                            class="rounded float-left border"></img>
+                        <img src="<?php echo get_theme_file_uri("./sources/tuv-iso-140012004-logo.png"); ?>" width="100%" height="225" class="rounded float-left border"></img>
                         <title>Placeholder</title>
-                        <rect width="100%" height="100%" fill="#55595c"></rect><text class="text-center" x="50%" y="50%"
-                            fill="#eceeef" dy=".3em">iso14001:2015</text></svg>
+                        <rect width="100%" height="100%" fill="#55595c"></rect><text class="text-center" x="50%" y="50%" fill="#eceeef" dy=".3em">iso14001:2015</text></svg>
                         <div class="card-body">
                             <p class="card-text">گواهینامه iso14001:2015 نمایند.البته استاندارد ایزو 14001:2015
                                 تاحدود
@@ -107,11 +109,9 @@
                 </div>
                 <div class="col-md-4 ">
                     <div class="card mb-4 shadow-sm">
-                        <img src="./sources/tuv-iso-140012004-logo.png" width="100%" height="225"
-                            class="rounded float-left border"></img>
+                        <img src="<?php echo get_theme_file_uri("./sources/tuv-iso-140012004-logo.png"); ?>" width="100%" height="225" class="rounded float-left border"></img>
                         <title>Placeholder</title>
-                        <rect width="100%" height="100%" fill="#55595c"></rect><text class="text-center" x="50%" y="50%"
-                            fill="#eceeef" dy=".3em">iso14001:2015</text></svg>
+                        <rect width="100%" height="100%" fill="#55595c"></rect><text class="text-center" x="50%" y="50%" fill="#eceeef" dy=".3em">iso14001:2015</text></svg>
                         <div class="card-body">
                             <p class="card-text">گواهینامه iso14001:2015 نمایند.البته استاندارد ایزو 14001:2015
                                 تاحدود
@@ -123,11 +123,9 @@
                 </div>
                 <div class="col-md-4 "">
                     <div class=" card mb-4 shadow-sm">
-                    <img src="./sources/tuv-iso-140012004-logo.png" width="100%" height="225"
-                        class="rounded float-left border"></img>
+                    <img src="<?php echo get_theme_file_uri("./sources/tuv-iso-140012004-logo.png"); ?>" width="100%" height="225" class="rounded float-left border"></img>
                     <title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="#55595c"></rect><text class="text-center" x="50%" y="50%"
-                        fill="#eceeef" dy=".3em">iso14001:2015</text></svg>
+                    <rect width="100%" height="100%" fill="#55595c"></rect><text class="text-center" x="50%" y="50%" fill="#eceeef" dy=".3em">iso14001:2015</text></svg>
                     <div class="card-body">
                         <p class="card-text">گواهینامه iso14001:2015 نمایند.البته استاندارد ایزو 14001:2015
                             تاحدود
@@ -148,11 +146,9 @@
             <div class="row">
                 <div class="col-md-4">
                     <div class="card mb-4 shadow-sm">
-                        <img src="./sources/tuv-iso-140012004-logo.png" width="100%" height="225"
-                            class="rounded float-left border"></img>
+                        <img src="<?php echo get_theme_file_uri("./sources/tuv-iso-140012004-logo.png"); ?>" width="100%" height="225" class="rounded float-left border"></img>
                         <title>Placeholder</title>
-                        <rect width="100%" height="100%" fill="#55595c"></rect><text class="text-center" x="50%" y="50%"
-                            fill="#eceeef" dy=".3em">iso14001:2015</text></svg>
+                        <rect width="100%" height="100%" fill="#55595c"></rect><text class="text-center" x="50%" y="50%" fill="#eceeef" dy=".3em">iso14001:2015</text></svg>
                         <div class="card-body">
                             <p class="card-text">گواهینامه iso14001:2015 نمایند.البته استاندارد ایزو 14001:2015
                                 تاحدود
@@ -164,11 +160,9 @@
                 </div>
                 <div class="col-md-4">
                     <div class="card mb-4 shadow-sm">
-                        <img src="./sources/tuv-iso-140012004-logo.png" width="100%" height="225"
-                            class="rounded float-left border"></img>
+                        <img src="<?php echo get_theme_file_uri("./sources/tuv-iso-140012004-logo.png"); ?>" width="100%" height="225" class="rounded float-left border"></img>
                         <title>Placeholder</title>
-                        <rect width="100%" height="100%" fill="#55595c"></rect><text class="text-center" x="50%" y="50%"
-                            fill="#eceeef" dy=".3em">iso14001:2015</text></svg>
+                        <rect width="100%" height="100%" fill="#55595c"></rect><text class="text-center" x="50%" y="50%" fill="#eceeef" dy=".3em">iso14001:2015</text></svg>
                         <div class="card-body">
                             <p class="card-text">گواهینامه iso14001:2015 نمایند.البته استاندارد ایزو 14001:2015
                                 تاحدود
@@ -180,11 +174,9 @@
                 </div>
                 <div class="col-md-4">
                     <div class="card mb-4 shadow-sm">
-                        <img src="./sources/tuv-iso-140012004-logo.png" width="100%" height="225"
-                            class="rounded float-left border"></img>
+                        <img src="<?php echo get_theme_file_uri("./sources/tuv-iso-140012004-logo.png"); ?>" width="100%" height="225" class="rounded float-left border"></img>
                         <title>Placeholder</title>
-                        <rect width="100%" height="100%" fill="#55595c"></rect><text class="text-center" x="50%" y="50%"
-                            fill="#eceeef" dy=".3em">iso14001:2015</text></svg>
+                        <rect width="100%" height="100%" fill="#55595c"></rect><text class="text-center" x="50%" y="50%" fill="#eceeef" dy=".3em">iso14001:2015</text></svg>
                         <div class="card-body">
                             <p class="card-text">گواهینامه iso14001:2015 نمایند.البته استاندارد ایزو 14001:2015
                                 تاحدود
@@ -206,11 +198,9 @@
 
                 <div class="col-md-4">
                     <div class="card mb-4 shadow-sm">
-                        <img src="./sources/tuv-iso-140012004-logo.png" width="100%" height="225"
-                            class="rounded float-left border"></img>
+                        <img src="<?php echo get_theme_file_uri("./sources/tuv-iso-140012004-logo.png"); ?>" width="100%" height="225" class="rounded float-left border"></img>
                         <title>Placeholder</title>
-                        <rect width="100%" height="100%" fill="#55595c"></rect><text class="text-center" x="50%" y="50%"
-                            fill="#eceeef" dy=".3em">iso14001:2015</text></svg>
+                        <rect width="100%" height="100%" fill="#55595c"></rect><text class="text-center" x="50%" y="50%" fill="#eceeef" dy=".3em">iso14001:2015</text></svg>
                         <div class="card-body">
                             <p class="card-text">گواهینامه iso14001:2015 نمایند.البته استاندارد ایزو 14001:2015
                                 تاحدود
@@ -222,11 +212,9 @@
                 </div>
                 <div class="col-md-4">
                     <div class="card mb-4 shadow-sm">
-                        <img src="./sources/tuv-iso-140012004-logo.png" width="100%" height="225"
-                            class="rounded float-left border"></img>
+                        <img src="<?php echo get_theme_file_uri("./sources/tuv-iso-140012004-logo.png"); ?>" width="100%" height="225" class="rounded float-left border"></img>
                         <title>Placeholder</title>
-                        <rect width="100%" height="100%" fill="#55595c"></rect><text class="text-center" x="50%" y="50%"
-                            fill="#eceeef" dy=".3em">iso14001:2015</text></svg>
+                        <rect width="100%" height="100%" fill="#55595c"></rect><text class="text-center" x="50%" y="50%" fill="#eceeef" dy=".3em">iso14001:2015</text></svg>
                         <div class="card-body">
                             <p class="card-text">گواهینامه iso14001:2015 نمایند.البته استاندارد ایزو 14001:2015
                                 تاحدود
@@ -238,11 +226,9 @@
                 </div>
                 <div class="col-md-4">
                     <div class="card mb-4 shadow-sm">
-                        <img src="./sources/tuv-iso-140012004-logo.png" width="100%" height="225"
-                            class="rounded float-left border"></img>
+                        <img src="<?php echo get_theme_file_uri("./sources/tuv-iso-140012004-logo.png"); ?>" width="100%" height="225" class="rounded float-left border"></img>
                         <title>Placeholder</title>
-                        <rect width="100%" height="100%" fill="#55595c"></rect><text class="text-center" x="50%" y="50%"
-                            fill="#eceeef" dy=".3em">iso14001:2015</text></svg>
+                        <rect width="100%" height="100%" fill="#55595c"></rect><text class="text-center" x="50%" y="50%" fill="#eceeef" dy=".3em">iso14001:2015</text></svg>
                         <div class="card-body">
                             <p class="card-text">گواهینامه iso14001:2015 نمایند.البته استاندارد ایزو 14001:2015
                                 تاحدود
@@ -320,7 +306,7 @@
             </div>
             <div class="col-12 p-2 pr-5">
                 <blockquote class="blockquote text-right">
-                    <img class="" src="./sources/Logo.png" alt="">
+                    <img class="" src="<?php echo get_theme_file_uri("./sources/logo.png"); ?>" alt="">
                     <footer class="blockquote-footer text-right">کلیه حقوق مادی و معنوی سایت محفوظ و متعلق
                         به گروه صنایع غذایی ریحانه می باشد. 1398 ©</footer>
                 </blockquote>
@@ -330,17 +316,14 @@
 
 
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-        </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
-        </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-        </script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
     <script src="https://unpkg.com/scrollreveal"></script>
     <script src="js/scripts.js"></script>
 </body>
-
+<?php wp_footer(); ?>
 </html>
